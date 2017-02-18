d5d88dae2040b082c6165cf58a3e9333b4006679
dardoCamanio <dardocam@gmail.com>
2017-02-18 05:25:11 -0300

    co1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -88,4 +88,27 @@ class Welcome extends CI_Controller {
 		
 		echo $data['mensaje'];
 	}
+	public function get_contact()
+	{
+		
+		// convert_accented_characters($nombre)
+		$data['mensaje'] = "hola";//$this->security->xss_clean(strip_tags($_POST['txt-msg']));
+		/*$data['email'] = $this->security->xss_clean(strip_tags($_POST['txt-email']));
+		$data['nombre'] = $this->security->xss_clean(strip_tags($_POST['txt-email']));
+		$data['agente'] = $this->agente();
+		$data['ip_remota'] = $this->input->ip_address();
+		$txt = "\n".$data['ip_remota'].";".$data['email'].";".$data['nombre'].";".$data['agente'].";".$data['mensaje'].";";
+		write_file('application/logs/data.txt', $txt, 'a+');
+		//$this->load->view('welcome_prueba',$data);
+		$this->load->library('email');
+		
+		$this->email->from('email@email.com', 'Name');
+		$this->email->to('ecam.construcciones@gmail.com');
+		$this->email->subject('subject');
+		$this->email->message('message'.$data['mensaje']);
+		
+		$this->email->send();*/
+		
+		echo $data['mensaje'];
+	}
 }
